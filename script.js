4baa5ecf7ff88f8b7d54a6db7fae358215625f3f
PR7175Z <aadimpraz@gmail.com>
2024-04-17 22:58:46 +0545

    calculator coming together

--- script.js
+++ script.js
@@ -4,23 +4,30 @@ var subview = document.getElementById('subdisplay');
 
 var num = document.getElementsByClassName('number-btn');
 var sign = document.getElementsByClassName('sign-btn');
-var result=document.getElementById('result')
+var result=document.getElementById('result');
 
 result.addEventListener('click', function(){
-    
-})
-
-signview.addEventListener('change', function(){
-    console.log('signchange')
+    view.innerHTML = eval(subview.innerHTML + signview.innerHTML + view.innerHTML);
+    signview.innerHTML = "";
+    subview.innerHTML = "";
 })
 
 for (let i = 0; i < num.length; i++) {
     num[i].addEventListener('click', function(){
-        view.append(this.value)
+        view.append(this.value);
     })
 }
+
 for (let i = 0; i < sign.length; i++) {
     sign[i].addEventListener('click', function(){
-        signview.innerHTML = this.value
-    })
+        signview.innerHTML = this.value;
+        subview.innerHTML = view.innerHTML;
+        view.innerHTML = "";
+    });
 }
+
+clearBtn.addEventListener('click', function(){
+    signview.innerHTML = "";
+    subview.innerHTML = "";
+    view.innerHTML = "";
+});
